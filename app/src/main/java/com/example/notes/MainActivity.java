c84c92140c72b72d6412cf08e7bcb74f31438bc3
letheos <nintendoplayeraddict@gmail.com>
2024-02-16 14:06:46 +0100

    premier commit

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button créernote = findViewById(R.id.ajouterNote);
+        créernote.setOnClickListener(this::créernote);
+    }
+    private void créernote(View view){
+        Intent intent = new Intent(this, Creationnote.class);
+        startActivity(intent);
     }
 }
